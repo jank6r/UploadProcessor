d762b21ec8c5ff2d0deda040d48cabc832456945
Kirchner, Jan <jan.kirchner@daimler.com>
2015-11-12 12:13:59 +0100

    Set working dir for processes

--- src/main/java/de/jk/upload/command/ExternalCommandFactory.java
+++ src/main/java/de/jk/upload/command/ExternalCommandFactory.java
@@ -1,15 +1,25 @@
 package de.jk.upload.command;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalCommandFactory {
 
     private int sequence = 0;
+    private File workingDirectory;
+
+    @Value("${uploadPath}")
+    public void setWorkingDirectoryPath(String path) {
+        workingDirectory = new File(path);
+    }
 
     public ExternalCommand create(String... commandStr) {
         ExternalCommand command = new ExternalCommand(commandStr);
         command.setId(sequence++);
+        command.setWorkingDirectory(workingDirectory);
 
         return command;
     }
